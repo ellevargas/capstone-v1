75946c90aa2c535a8a266cc05e6d2476b386f879
Jennie Buechner <janina.buechner@gmail.com>
2016-03-02 11:59:25 -0800

    update company list component

--- app/components/company-list.component.ts
+++ app/components/company-list.component.ts
@@ -1,6 +1,16 @@
-import { Component } from 'angular2/core';
+// Angular imports
+import { Component, OnInit } from 'angular2/core';
+import { HTTP_PROVIDERS }    from 'angular2/http';
+// Rxjs imports
+import { Observable } from 'rxjs/Observable';
+// Project imports
 import { Company } from '../models/company';
 import { CompanyService } from '../services/company.service';
+// In-memory web api imports
+import { provide } from 'angular2/core';
+import { XHRBackend } from 'angular2/http';
+import { InMemoryBackendService, SEED_DATA } from 'a2-in-memory-web-api/core';
+import { CompanyData } from '../mocks/company-data';
 
 @Component({
 	selector: "company-list",
@@ -11,14 +21,29 @@ import { CompanyService } from '../services/company.service';
 		</li>
 	</ul>
   `,
+  providers: [HTTP_PROVIDERS, CompanyService,
+		// in-memory web api providers
+		provide(XHRBackend, { useClass: InMemoryBackendService }), // in-mem server
+		provide(SEED_DATA, { useClass: CompanyData }) // in-mem server data
+	]
 })
 
-export class CompanyListComponent {
+export class CompanyListComponent implements OnInit {
+	errorMessage: string;
 	public companies: Company[];
 	public selectedCompany: Company;
 
-	constructor(companyService: CompanyService) {
-		this.companies = companyService.getCompanies();
+	constructor(private _companyService: CompanyService) {}
+
+	ngOnInit() {
+		this.getCompanies();
+	}
+
+	getCompanies() {
+		this._companyService.getCompanies()
+			.subscribe(
+				companies => this.companies = companies,
+				error => this.errorMessage = <any>error)
 	}
 
 	onSelect(company: Company) {
