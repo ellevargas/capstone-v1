0c2a5203290b241cda608ee716ba03c60e861a60
Jennie Buechner <janina.buechner@gmail.com>
2016-03-01 16:33:38 -0800

    create mock-companies

---
+++ app/mocks/mock-companies.ts
@@ -0,0 +1,54 @@
+import { Company } from '../models/company';
+
+export var COMPANY: Company[] = [
+  {
+    "id": 1,
+    "name": "Expedia",
+    "street": "",
+    "city": "Bellevue",
+    "state": "WA",
+    "zip": 98004,
+    "website": "http://www.lifeatexpedia.com/",
+    "logo": "expedia.jpg"
+  },
+  {
+    "id": 2,
+    "name": "Moz",
+    "street": "1100 2nd Ave #500",
+    "city": "Seattle",
+    "state": "WA",
+    "zip": 98101,
+    "website": "https://moz.com/about/jobs",
+    "logo": "Moz-Logo.jpg"
+  },
+  {
+    "id": 3,
+    "name": "Liquid Planner",
+    "street": "2324 Eastlake Ave E #500",
+    "city": "Seattle",
+    "state": "WA",
+    "zip": 98102,
+    "website": "http://www.liquidplanner.com/careers/",
+    "logo": "liquid_planner.jpg"
+  },
+  {
+    "id": 4,
+    "name": "Marchex",
+    "street": "520 Pike St # 2000",
+    "city": "Seattle",
+    "state": "WA",
+    "zip": 98101,
+    "website": "http://www.marchex.com/company/careers/",
+    "logo": "marchex_logo.png"
+  },
+  {
+    "id": 5,
+    "name": "Apex Learning Inc.",
+    "street": "1215 4th Ave #1500",
+    "city": "Seattle",
+    "state": "WA",
+    "zip": 98101,
+    "website": "https://www.apexlearning.com/",
+    "logo": "apex_learning.jpeg"
+  },
+];
