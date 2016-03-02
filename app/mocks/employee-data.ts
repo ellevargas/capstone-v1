1d415f1d11ba5f14c5d3833e104c0157e8c1e356
Jennie Buechner <janina.buechner@gmail.com>
2016-03-02 12:01:18 -0800

    create employee data file

---
+++ app/mocks/employee-data.ts
@@ -0,0 +1,66 @@
+export class EmployeeData {
+	createDb() {
+		let employees = [
+			{
+				"id": 1,
+				"name": "Aisha Mathews",
+				"position": "Early Career Programs Recruiter",
+				"email": "amathews@expedia.com",
+				"twitter": "",
+				"linked_in_url": "https://www.linkedin.com/in/aishamathews",
+				"image": "",
+				"company_id": 1
+			},
+			{
+				"id": 2,
+				"name": "Nasreen Abdulijaleel",
+				"position": "Technology Director",
+				"email": "nabduljaleel@expedia.com",
+				"twitter": "",
+				"linked_in_url": "https://www.linkedin.com/in/nasreenabduljaleel",
+				"image": "",
+				"company_id": 1
+			},
+			{
+				"id": 3,
+				"name": "Kelsey Foley",
+				"position": "Sr. SDET/Software Engineer",
+				"email": "kelsey@moz.com",
+				"twitter": "",
+				"linked_in_url": "https://www.linkedin.com/in/kelsey-anderson-foley-914b751",
+				"image": "",
+				"company_id": 2
+			},
+			{
+				"id": 4,
+				"name": "David Weiser",
+				"position": "Software Engneer",
+				"email": "davidann@moz.com",
+				"twitter": "",
+				"linked_in_url": "https://www.linkedin.com/in/david-weiser-238b6223",
+				"image": "",
+				"company_id": 3
+			},
+			{
+				"id": 5,
+				"name": "Adam Edwards",
+				"position": "Engineering General Manager",
+				"email": "adamed@chef.io",
+				"twitter": "@adamedx",
+				"linked_in_url": "https://www.linkedin.com/in/adamedx",
+				"image": "",
+				"company_id": 4
+			},
+			{
+				"id": 6,
+				"name": "Wendy McIntosh",
+				"position": "Director; Global Talent/Recruiting",
+				"email": "mcintoshw@getchef.com",
+				"twitter": "",
+				"linked_in_url": "https://www.linkedin.com/in/wendymcintosh1",
+				"image": "",
+				"company_id": 5
+			},
+		];
+		return { employees };
+}
