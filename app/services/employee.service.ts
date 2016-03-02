1ff9a5436ff232b439e9334c27e19b1d525df82b
Jennie Buechner <janina.buechner@gmail.com>
2016-03-02 12:07:20 -0800

    update employee service to use http

--- app/services/employee.service.ts
+++ app/services/employee.service.ts
@@ -1,9 +1,29 @@
+// Angular imports
 import { Injectable } from 'angular2/core';
+import { Http, Response } from 'angular2/http';
+// Rxjs imports
+import { Observable } from 'rxjs/Observable';
+// Project imports
 import { Employee } from '../models/employee';
-import { EMPLOYEES } from '../mocks/mock-employees';
 
 @Injectable()
 
 export class EmployeeService {
-	getEmployees() { return EMPLOYEES; }
+	constructor(private http:Http){}
+
+	private _employeesUrl = 'app/employees';
+
+	private getEmployees() {
+		return this.http.get(this._employeesUrl)
+			.map(res => <Employee[]> res.json().data)
+			.do(data => console.log(data))
+			.catch(err => this.handleError(err));
+	}
+
+	private handleError(error: Response) {
+    // in a real world app, we may send the error to some remote logging infrastructure
+    // instead of just logging it to the console
+    console.error(error);
+    return Observable.throw(error.json().error || 'Server error');
+  }
 }
