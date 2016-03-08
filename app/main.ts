12d333ff4a94962e2f8c2a25b9fdd6b928dedf2d
Jennie Buechner <janina.buechner@gmail.com>
2016-03-08 14:54:05 -0800

    update main.ts for auth0

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,8 @@
-import {bootstrap}    from 'angular2/platform/browser'
+import {bootstrap}    from 'angular2/platform/browser';
+import {provide} from 'angular2/core';
+import {HTTP_PROVIDERS, Http} from 'angular2/http';
+import {AuthHttp, AuthConfig} from 'angular2-jwt';
+
 import {ROUTER_PROVIDERS} from 'angular2/router';
 
 // Add all operators to Observable
@@ -6,4 +10,13 @@ import 'rxjs/Rx';
 
 import {AppComponent} from './components/app.component'
 
-bootstrap(AppComponent, [ROUTER_PROVIDERS]);
+bootstrap(AppComponent, [
+	ROUTER_PROVIDERS,
+	HTTP_PROVIDERS,
+  provide(AuthHttp, {
+    useFactory: (http) => {
+      return new AuthHttp(new AuthConfig(), http);
+    },
+    deps: [Http]
+  }),
+]);
