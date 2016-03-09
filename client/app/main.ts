add8984b2c4b3cb98d6c1b30141a71b404ed8340
Jennie Buechner <janina.buechner@gmail.com>
2016-03-09 11:57:06 -0800

    move app to client directory

--- client/app/main.ts
+++ client/app/main.ts
@@ -1,3 +1,5 @@
+/// <reference path="../libs/angular2/typings/browser.d.ts" />
+
 import {bootstrap}    from 'angular2/platform/browser';
 import {provide} from 'angular2/core';
 import {AppComponent} from './components/app.component'
